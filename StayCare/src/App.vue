5c085942d53aad33e4890c4dda034cb740b9b76c
Finn-S123 <Finn.skattum@elev.ga.ntig.se>
2026-02-24 12:54:02 +0100

    Structure

--- StayCare/src/App.vue
+++ StayCare/src/App.vue
@@ -1,20 +1,8 @@
 <script setup></script>
 
 <template>
-  <div class="template">
-  <h1 class="text-4xl font-bold text-center mt-10 text-[#FF56B0]">Login for <span class="text-[#00F5F3]">StayCare</span> laundry management</h1>
-  <div class="flex justify-center items-center h-96">Login area </div>
-  </div>
 </template>
 
 <style scoped>
-.template {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  background: linear-gradient(to right, #ff56b0, #00f5f3);
-}
 </style>
  
