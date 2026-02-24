4cc16fea1f9bd87b40291548aac07728838cb1f9
Finn-S123 <Finn.skattum@elev.ga.ntig.se>
2026-02-24 13:02:58 +0100

    Routes

--- StayCare/src/App.vue
+++ StayCare/src/App.vue
@@ -1,6 +1,11 @@
-<script setup></script>
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+</script>
 
 <template>
+  <router-view />
 </template>
 
 <style scoped>
