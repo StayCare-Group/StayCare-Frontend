bb9f900c39b29e2a5c7d5e7ecc9136038f944525
Jesper Ahlström <143940213+KebabMumsare@users.noreply.github.com>
2026-03-02 15:09:00 +0100

    t fix

--- StayCare/src/Pages/Login.vue
+++ StayCare/src/Pages/Login.vue
@@ -4,6 +4,8 @@ import { useRouter } from 'vue-router'
 import { useI18n } from 'vue-i18n'
 import { useAuthStore } from '../stores/auth.js'
 
+const { t } = useI18n()
+
 const bubbles = computed(() =>
   Array.from({ length: 15 }, (_, i) => ({
     id: i,
